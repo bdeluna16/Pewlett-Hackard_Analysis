32e6665f7a84317ca0bf43563e50e44add3cbb41
bdeluna16 <67936161+bdeluna16@users.noreply.github.com>
2020-08-30 22:07:48 -0700

    Update Employee_Database_Challenge.sql

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -23,7 +23,6 @@ WHERE dept_name IN ('Sales');
 
 SELECT * FROM sales_retirement
 
-
 SELECT ce.emp_no,
 	ce.first_name,
 	ce.last_name,
@@ -38,11 +37,6 @@ WHERE dept_name IN ('Sales', 'Development')
 ORDER BY dept_name;
 
 SELECT * FROM sales_development_retirement
-
-SELECT COUNT(emp_no), dept_name
-FROM sales_development_retirement
-GROUP BY dept_name
-ORDER BY count DESC; 
 
 SELECT e.emp_no,
 		e.first_name,
@@ -72,3 +66,47 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+
+SELECT e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		ti.title
+INTO mentorship_req
+FROM employees as e
+INNER JOIN dept_employee as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no)
+emp_no,
+first_name,
+last_name,
+birth_date,
+from_date,
+to_date,
+title
+INTO mentorship_eligibility_clean
+FROM mentorship_req
+ORDER BY emp_no, to_date DESC
+
+
+SELECT emp_no,
+first_name,
+last_name,
+birth_date,
+from_date,
+to_date,
+title
+INTO mentorship_eligibility
+FROM mentorship_eligibility_clean
+ORDER BY to_date DESC, emp_no
+
+SELECT * FROM mentorship_eligibility
